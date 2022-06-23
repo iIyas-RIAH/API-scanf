aa6017a457c116c0d637e2bba3d0a6bb2dc98c3f
iIyas-RIAH <57155400+iIyas-RIAH@users.noreply.github.com>
2022-06-23 20:27:45 +0100

    Update sqlConnexion.php

--- sqlConnexion.php
+++ sqlConnexion.php
@@ -1,5 +1,5 @@
 <?php
-$connexion = mysqli_connect('localhost', 'abcglkvl_scanf', '11808192@Rs', 'abcglkvl_scanf');
+$connexion = mysqli_connect('eu-cdbr-west-02.cleardb.net', 'b261f7527db285', '29509f46', 'heroku_378e833f98f78f3');
 mysqli_set_charset($connexion, "utf8");
 if (!$connexion) {
     die("Connection failed: " . mysqli_connect_error());
